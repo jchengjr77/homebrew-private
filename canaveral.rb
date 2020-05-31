b71feff80a80ad2da2f862373e6b22cf5f597e5c
Jonathan Cheng <jonathanchengjr77@gmail.com>
2020-05-31 15:14:38 +0800

    Updated SHA256 to match the latest version.

--- canaveral.rb
+++ canaveral.rb
@@ -4,7 +4,9 @@ class Canaveral < Formula
   desc "Launch your new project effortlessly. Canaveral is a CLI for developers."
   homepage ""
   url "https://github.com/jchengjr77/canaveral/releases/download/v1.0.0/canaveral_darwin_amd64.tar.gz"
-  sha256 "550eafe9b6e77007ab5e0c57bc76e7752eff086de217fc21d10795fdb55e9499"
+  sha256 "ff0faa68fed0a3f5894ad20fcf5ef9fbecb2dc76696e258a2fea1631e523769f"
+
+  # depends_on "cmake" => :build
 
   def install
     bin.install "canaveral"
