28ec64c477fd522dcd4f02b4aab7f3861765203f
Jonathan Cheng <jonathanchengjr77@gmail.com>
2020-05-31 15:11:05 +0800

    Updated Homebrew formula to get new Canaveral version.

--- canaveral.rb
+++ canaveral.rb
@@ -3,7 +3,7 @@
 class Canaveral < Formula
   desc "Launch your new project effortlessly. Canaveral is a CLI for developers."
   homepage ""
-  url "https://github.com/jchengjr77/canaveral/releases/download/v0.6.0/canaveral_darwin_amd64.tar.gz"
+  url "https://github.com/jchengjr77/canaveral/releases/download/v1.0.0/canaveral_darwin_amd64.tar.gz"
   sha256 "550eafe9b6e77007ab5e0c57bc76e7752eff086de217fc21d10795fdb55e9499"
 
   def install
