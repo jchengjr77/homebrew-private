ad131c8921a3f0e1558d2ed1911d1f19056342d4
Jonathan Cheng <jonathanchengjr77@gmail.com>
2020-09-25 23:04:21 +0800

    Updated URL for download

--- speedtracker.rb
+++ speedtracker.rb
@@ -4,7 +4,7 @@
 class Speedtracker < Formula
   desc "Collect basic analytics on your network performance over time."
   homepage ""
-  url "https://github.com/jchengjr77/speedtracker/releases/download/v1.0.0/speedtracker.tar.gz"
+  url "https://github.com/jchengjr77/speedtracker/releases/download/v1.0.0/speedtracker_mac_linux.tar.gz"
   sha256 "23ec4a297b6fac1c26fcb21ab39571d6eb2af49e8519716015ff8ba3ae0e41cf"
 
   # depends_on "cmake" => :build
